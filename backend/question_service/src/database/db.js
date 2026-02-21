ada76a4b06cbafc31eb13bbb0936f7bacd2a9fc2
obrona <tlim8772@gmail.com>
2026-02-21 09:40:59 +0800

    add update question database method

--- backend/question_service/src/database/db.js
+++ backend/question_service/src/database/db.js
@@ -27,6 +27,27 @@ export async function get_question_by_id(id) {
     return result.rows[0];
 }
 
+export async function get_all_questions_without_body() {
+    const result = await pool.query(
+        `
+        SELECT 
+            q.title, 
+            q.difficulty, 
+            COALESCE(ARRAY_AGG(qt.tag) FILTER (WHERE qt.tag IS NOT NULL), '{}') as tags
+        FROM questions q
+        LEFT JOIN question_tag qt on q.id = qt.question_id
+        GROUP BY q.id
+        ORDER BY q.id
+        `
+        , []);
+
+    return result.rows;
+}
+
+export async function create_question_from_obj(obj) {
+    return await create_question(obj.title, obj.difficulty, obj.tags, obj.body);
+}
+
 // returns the id of the newly inserted question
 export async function create_question(title, difficulty, tags, body) {
     const result = await pool.query(
@@ -49,3 +70,31 @@ export async function delete_question(id) {
     await pool.query(`DELETE FROM questions WHERE id = $1`, [id]);
 }
 
+// obj must have { title, difficulty, tags, body }
+export async function update_question(id, obj) {
+    const client = await pool.connect();
+   
+    await client.query("BEGIN");
+
+    await client.query(
+        `DELETE FROM question_tag WHERE question_id = $1`,
+        [id]
+    );
+
+    await client.query(
+        `INSERT INTO question_tag (question_id, tag)
+            SELECT $1, unnest($2::TEXT[])`,
+        [id, obj.tags]
+    );
+
+    await client.query(
+        `UPDATE questions
+            SET title = $1,
+                difficulty = $2,
+                body = $3
+            WHERE id = $4`,
+        [obj.title, obj.difficulty, obj.body, id]
+    );
+
+    await client.query("COMMIT");   
+}
--- backend/question_service/src/database/db.test.js
+++ backend/question_service/src/database/db.test.js
@@ -1,5 +1,5 @@
 import { test, expect, beforeEach, afterEach, afterAll } from 'vitest';
-import { create_question, get_question_by_id, pool } from './db';
+import { create_question, create_question_from_obj, get_all_questions_without_body, get_question_by_id, pool, update_question } from './db';
 
 beforeEach(async () => {
     await pool.query(`
@@ -44,4 +44,51 @@ test('create question with no tags', async () => {
 test('get non-existant question', async () => {
     const result = await get_question_by_id(6969);
     expect(result).toBeUndefined();
+})
+
+test('get all questions', async () => {
+    const q1 = {
+        title: 't1',
+        difficulty: 'medium',
+        tags: ['pq', 'dp'],
+        body: 'hello world',
+    }
+
+    const q2 = {
+        title: 't2',
+        difficulty: 'hard',
+        tags: ['pq', 'heap'],
+        body: 'hello world',
+    }
+
+    await create_question_from_obj(q1);
+    await create_question_from_obj(q2);
+    
+    const result = await get_all_questions_without_body();
+    expect(result).toHaveLength(2);
+    
+    delete q1.body;
+    delete q2.body;
+    expect(result).toMatchObject([q1, q2]);
+})
+
+test('update question', async () => {
+    const q1 = {
+        title: 't1',
+        difficulty: 'medium',
+        tags: ['pq', 'dp'],
+        body: 'hello world',
+    }
+
+    const q2 = {
+        title: 't2',
+        difficulty: 'hard',
+        tags: ['pq', 'heap'],
+        body: 'hello world',
+    }
+
+    const id = await create_question_from_obj(q1);
+    await update_question(id, q2);
+    const question = await get_question_by_id(id);
+    expect(question).toMatchObject(q2);
 })
