33a03f7bd42e03c3d643da9a5baf5250e73172ab
CliffKoes <cliffkoes@gmail.com>
2026-03-09 02:18:53 +0800

    Set up collaboration service test

---
+++ backend/collaboration_service/src/App.jsx
@@ -0,0 +1,39 @@
+import { useRef } from 'react'
+import './App.css'
+import Editor from '@monaco-editor/react'
+import * as Y from "yjs"
+import { WebrtcProvider } from "y-webrtc"
+import { MonacoBinding } from "y-monaco"
+
+function App() {
+  const editorRef = useRef(null)
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor
+
+    // Init Yjs 
+    const ydoc = new Y.Doc()
+
+    // P2P connection using webrtc
+    const provider = new WebrtcProvider("roomname", ydoc)
+    const yText = ydoc.getText("monaco") // use key "monaco" and bind to editor
+
+    // Bind yjs to monaco
+    const binding = new MonacoBinding(
+      yText, 
+      editorRef.current.getModel(), 
+      new Set([editorRef.current]), 
+      provider.awareness) // shared data for each room
+  }
+
+  return (
+    <Editor
+      height="100vh"
+      width="100vw"
+      theme="vs-dark"
+      onMount={handleEditorDidMount}
+    />
+  )
+}
+
+export default App
