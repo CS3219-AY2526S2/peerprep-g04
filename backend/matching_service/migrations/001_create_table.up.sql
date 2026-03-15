47743007bbb5c42fa2aa353f8e5f96221255ab8a
MA YITAO <2991588053@QQ.COM>
2026-03-15 17:51:28 +0800

    Revert "Collaboration service java"

--- backend/matching_service/migrations/001_create_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE matches (
-    id          INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user1_id    INT NOT NULL,
-    user2_id    INT NOT NULL,
-    topic       TEXT NOT NULL,
-    difficulty  TEXT NOT NULL,
-    question_id INT,
-    created_at  TIMESTAMPTZ DEFAULT NOW()
-);
