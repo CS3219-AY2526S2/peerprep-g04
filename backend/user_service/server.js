f6c7df6a1538fc78f31964d88a8a38ab559bdee3
obrona <tlim8772@gmail.com>
2026-03-25 14:53:02 +0800

    commit

--- backend/user_service/server.js
+++ backend/user_service/server.js
@@ -1,9 +1,10 @@
 import { app } from "./src/app.js";
-import { create_user, get_user_by_username } from "./src/database/db.js";
+import { create_user, get_user_by_email, get_user_by_username } from "./src/database/db.js";
 
 async function create_tim() {
     const res = await get_user_by_username('tim');
-    if (res) return;
+    const res2 = await get_user_by_email('tim@gmail.com');
+    if (res || res2) return;
     await create_user('tim', 'tim@gmail.com', 'abc', 'admin');
 }
 
