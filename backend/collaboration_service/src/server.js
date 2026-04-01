5fc3fa80baec0344b653cb213bb529c99578fe51
CliffKoes <cliffkoes@gmail.com>
2026-04-01 13:58:41 +0800

    Collab microservice hotfix

--- backend/collaboration_service/src/server.js
+++ backend/collaboration_service/src/server.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-import WebSocket from 'ws'
+import { WebSocketServer } from 'ws'
 import http from 'http'
 import * as number from 'lib0/number'
 import { setupWSConnection } from './utils.js'
 import jwt from 'jsonwebtoken';
 
-const wss = new WebSocket.Server({ noServer: true })
-const host = process.env.HOST || 'localhost'
+const wss = new WebSocketServer({ noServer: true })
+const host = process.env.HOST || '0.0.0.0'
 const port = number.parseInt(process.env.PORT || '3004')
 
 const server = http.createServer((_request, response) => {
