5b0632a8e704c2ffa1e7328c3a7dc84f9ace6323
obrona <tlim8772@gmail.com>
2026-04-06 12:39:32 +0800

    commit

--- backend/chat_service/server.js
+++
@@ -1,8 +0,0 @@
-import http from 'http'
-
-const server = http.createServer();
-
-server.listen(3004, (err) => {
-    if (err) console.error(err);
-    console.log(`Matching service listening on port 3004`);
-});
