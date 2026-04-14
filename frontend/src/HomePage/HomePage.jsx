26dd39d564fbffc69d0933e958cb80151fc5d6ae
EthanLoww <lowethan11@gmail.com>
2026-04-14 21:43:51 +0800

    Change start button in home page

--- frontend/src/HomePage/HomePage.jsx
+++ frontend/src/HomePage/HomePage.jsx
@@ -1,17 +1,10 @@
 import styles from './HomePage.module.css';
-import { useNavigate } from "react-router";
 import { DashboardPage } from '../DashboardPage/DashboardPage';
-import Button from "@mui/material/Button";
 
 export function HomePage() {
-  const navigate = useNavigate();
-
   return (
     <div className={styles.main}>
       <DashboardPage />
-      <div className={styles.startRow}>
-        <Button variant='outlined' onClick={() => navigate('/signed-in/match')}>Start</Button>
-      </div>
     </div>
   );
 }
